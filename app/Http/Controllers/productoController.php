1db2b60e9ad1a3e203f00da009e5167adfb36cde
Mario Alberto Campo Londoño <mariocampo.dev@gmail.com>
2024-08-03 01:31:09 -0500

    Funcion listar todos los productos

--- app/Http/Controllers/productoController.php
+++ app/Http/Controllers/productoController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Producto;
 use Illuminate\Http\Request;
 
 class productoController extends Controller
 {
     public function index()
     {
-        return 'Listado de productos desde el controlador';
+        $productos = Producto::all();
+
+        if ($productos->isEmpty()) {
+            $data = [
+                'message' => 'No existen productos en este momento.',
+                'status' => 200
+            ];
+
+            return response()->json($data, 200);
+        }
+
+        $data = [
+            'productos' => $productos,
+            'status' => 200
+        ];
+        
+        return response()->json($data, 200);
     }
 }
