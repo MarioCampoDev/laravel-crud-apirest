e5d8d9c0262135dd7693288e0863f31292a7de41
Mario Alberto Campo Londoño <mariocampo.dev@gmail.com>
2024-08-03 01:11:09 -0500

    Rutas crud
    Generacion de rutas para crud api rest

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/productos', function () {
     return 'Listado de productos';
 });
+
+Route::get('/productos/{id}', function () {
+    return 'Listado de producto';
+});
+
+Route::post('/productos', function () {
+    return 'Creacion de productos';
+});
+
+Route::put('/productos/{id}', function () {
+    return 'Actualizacion de productos';
+});
+
+Route::delete('/productos/{id}', function () {
+    return 'Borrado de productos';
+});
